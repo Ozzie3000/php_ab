45936810fd7ddf068ecb5c2b19bb6a7a759940e1
Ozzie3000 <ozzieboy19@hotmail.com>
2014-08-26 11:46:53 -0400

    heredoc php

---
+++ heredoc.php
@@ -0,0 +1,44 @@
+<?php
+
+$foo = <<<EOD
+This is a string created using heredoc syntax.
+It can span multiple lines, use "quotes" without having to
+escape them, and it'll allow apostrohes, and $variables.
+
+Special characters are still supporte as well.
+EOD;
+
+?>
+
+
+<?php
+$str = <<<EOD
+Example of string
+spanning multiple lines
+using heredoc syntax.
+EOD;
+
+/* More complex example, with variables. */
+class foo
+{
+    var $foo;
+    var $bar;
+
+    function foo()
+    {
+        $this->foo = 'Foo';
+        $this->bar = array('Bar1', 'Bar2', 'Bar3');
+    }
+}
+
+$foo = new foo();
+$name = 'MyName';
+
+echo <<<EOT
+My name is "$name". I am printing some $foo->foo.
+Now, I am printing some {$foo->bar[1]}.
+This should print a capital 'A': \x41
+EOT;
+?>
+
+
