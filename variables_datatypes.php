9542791c29b97e39d56dbc5973fced76677e05e4
Ozzie3000 <ozzieboy19@hotmail.com>
2014-08-26 10:31:23 -0400

    variables and datatypes

---
+++ variables_datatypes.php
@@ -0,0 +1,7 @@
+<h1>Variables and data types</h1>
+
+String, Integer, Float, Boolean, Array, Object
+
+$foo = "3"; // Consider a string
+$bar = $foo + 2; // foo is converted to an integer to be able to add them together.
+
