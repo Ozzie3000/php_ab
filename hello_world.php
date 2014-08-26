e9b466170f8e7b2513a51fd7dd0ea45d15772154
Ozzie3000 <ozzieboy19@hotmail.com>
2014-08-26 11:49:01 -0400

    hellow world

--- hello_world.php
+++ hello_world.php
@@ -1,3 +1,13 @@
 <?php
 	echo "Hello World";
 ?>
+
+<?php
+$foo = <<<EOD
+This is a string created using heredoc syntax.
+It can span multiple lines, use "quotes" without having to
+escape them, and it'll allow apostrohes, and $variables.
+
+Special characters are still supporte as well.
+EOD;
+?>
